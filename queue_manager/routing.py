81553bb2db955c4e5cbf0a3a3e1c2f79058c981e
saikaushik666 <SSridhara01@indianatech.net>
2025-04-28 17:18:51 -0400

    Delete queue_manager directory

--- queue_manager/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumers import QueueConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/queue/$', QueueConsumer.as_asgi()),  # Match the WebSocket URL
-]
